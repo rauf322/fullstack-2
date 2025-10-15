e4087262a40ced68f7f7ea68466103a72692fce2
rauf322 <rauf.faizov.200271@gmail.com>
2025-10-16 00:57:45 +0200

    test header

---
+++ src/__test__/Header.test.tsx
@@ -0,0 +1,49 @@
+import Header from '@/components/Header'
+import { AuthProvider } from '@/context/AuthContext'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { expect, test, vi } from 'vitest'
+
+test('check if header is correct', async () => {
+  vi.mock('@tanstack/react-router', () => ({
+    Link: ({ children, to, ...props }: any) => (
+      <a href={to} {...props}>
+        {children}
+      </a>
+    ),
+    useNavigate: () => vi.fn(),
+  }))
+  render(
+    <AuthProvider>
+      <Header />
+    </AuthProvider>,
+  )
+
+  const links = screen.getAllByRole('link')
+  const linkHrefs = links.map((link) => link.getAttribute('href'))
+  const linkToRegister = screen.getByText('Register')
+  expect(linkHrefs).toEqual(['/', '/', '/ideas', '/register', '/login'])
+  expect(linkHrefs).toHaveLength(5)
+  fireEvent.click(linkToRegister)
+})
+
+// TODO TRY TO MAKE MEMORYHISTORY
+//
+// test('clicking link change URL', async () => {
+//   const history = createMemoryHistory({ initialEntries: ['/'] })
+//   const rootRoute = createRootRoute({
+//     component: () => (
+//       <AuthProvider>
+//         <Header />
+//       </AuthProvider>
+//     ),
+//   })
+//   const router = createRouter({
+//     routeTree: rootRoute,
+//     history,
+//   })
+//   render(<RouterProvider router={router} />)
+//
+//   const loginLink = screen.getByText('Login')
+//   fireEvent.click(loginLink)
+//   expect(history.location.pathname).toBe('/login')
+// })
